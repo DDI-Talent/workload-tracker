0aed7edad0b4051e9363d1a5b3c9b38a917ee7a0
Kasia Banas <kasia.banas@gmail.com>
2025-02-16 09:16:05 +0000

    add width parameter

--- workload_analysis_function.R
+++ workload_analysis_function.R
@@ -2,7 +2,7 @@ library(waffle)
 library(tidyverse)
 library(dutchmasters)
 
-create_waffle_plot <- function(file_name, column_names) {
+create_waffle_plot <- function(file_name, column_names, width = 5) {
   column_names <- enquo(column_names)
   workload_data_plot <- read_csv(file_name) %>% 
     filter(!is.na(Category)) %>% 
@@ -13,7 +13,7 @@ create_waffle_plot <- function(file_name, column_names) {
       aes(fill = fct_rev(Category), values = n)
     ) +
     geom_waffle(
-      n_rows = 5,
+      n_rows = width,
       size = 0.33, 
       colour = "white",
       flip = TRUE
@@ -33,7 +33,10 @@ create_waffle_plot(file_name = "Workload_2025.csv",
                    column_names = c("Week of 20/01", "Week of 27/01"))
 
 
-
+create_waffle_plot(file_name = "Workload_2025_4weeks.csv", 
+                   column_names = c("Week of 20/01", "Week of 27/01",
+                                    "Week of 03/02", "Week of 10/02"),
+                   width = 10)
 
 
 
