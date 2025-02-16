506cced1736a5e7dbf2a3c8086aa708a15038fd1
Kasia Banas <kasia.banas@gmail.com>
2025-02-16 09:06:25 +0000

    create function

---
+++ workload_analysis_function.R
@@ -0,0 +1,40 @@
+library(waffle)
+library(tidyverse)
+library(dutchmasters)
+
+create_waffle_plot <- function(file_name, column_names) {
+  column_names <- enquo(column_names)
+  workload_data_plot <- read_csv(file_name) %>% 
+    filter(!is.na(Category)) %>% 
+    rowwise() %>% 
+    mutate(total_time = sum(across(all_of(!!column_names)), na.rm = TRUE)) %>% 
+    count(Category, wt = total_time) %>% 
+    ggplot(
+      aes(fill = fct_rev(Category), values = n)
+    ) +
+    geom_waffle(
+      n_rows = 5,
+      size = 0.33, 
+      colour = "white",
+      flip = TRUE
+    ) +
+    # nice colour palette from the dutchmasters package
+    # scale_fill_brewer(palette = "Accent") also worked ok
+    scale_fill_dutchmasters(palette = "pearl_earring")+
+    coord_equal() +
+    theme_void() +
+    theme_enhance_waffle() +
+    guides(fill=guide_legend(title="Category"))
+  
+  workload_data_plot
+}
+
+create_waffle_plot(file_name = "Workload_2025.csv", 
+                   column_names = c("Week of 20/01", "Week of 27/01"))
+
+
+
+
+
+
+
